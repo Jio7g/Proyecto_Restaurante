7f1e9ff833db093bd84f306daef87d49acc4a83e
Jio7g <cramosg8@miumg.edu.gt>
2023-10-19 18:39:24 +0000

    cofiguracion del deploy en el servidor

---
+++ deploy/server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DJANGODIR=$(dirname $(cd `dirname $0` && pwd))
+DJANGO_SETTINGS_MODULE=pizza.settings
+cd $DJANGODIR
+source env/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+exec python manage.py runserver 0:8000
--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\cramo\onedrive\escritorio\proyectofinal\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
